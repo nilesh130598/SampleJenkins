de9be583e9f98d3299b09a466cbf2c3c2791f9b6
Nilesh <patilnilesh77881432@gmail.com>
2022-02-24 16:41:54 +0530

    second commit

--- src/main/java/com/example/DemoApplication.java
+++ src/main/java/com/example/DemoApplication.java
@@ -20,6 +20,7 @@ public class DemoApplication {
 	}
 	public static void main(String[] args) {
 		logger.info("Application executed....");
+		logger.info("Application executed second time....");
 		SpringApplication.run(DemoApplication.class, args);
 	}
 	
